70f895ffc7e23f57bca306d2be365a2bedafe98d
molarmanful <benjinpang@gmail.com>
2024-03-04 12:13:16 -0500

    nice

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,29 +29,33 @@
     history.scrollRestoration = 'manual'
   }
 
-  onMount(() => {
-    for (let c of document.querySelectorAll('*:not([data-tag])')) {
-      let t = c.tagName
-      if (t) {
-        let a = document.createElement('span')
-        a.dataset.tag = ''
+  let deco = () => {
+    for (let el of document.querySelectorAll('*')) {
+      let t = el.tagName
+      if (t == 'X-TAG') {
+        el.remove()
+      } else if (t == 'X-NODE') {
+        el.replaceWith(el.querySelector('[data-orig]'))
+      } else if (t) {
+        let a = document.createElement('x-tag')
         a.innerText = `<${t}${
-          c.hasAttributes()
+          el.hasAttributes()
             ? ' ' +
-              [...c.attributes].map(a => a.name + '=' + JSON.stringify(a.value))
-                .join` `
+              [...el.attributes].map(
+                a => a.name + '=' + JSON.stringify(a.value)
+              ).join` `
             : ''
         }>`
 
-        let b = document.createElement('span')
-        b.dataset.tag = ''
+        let b = document.createElement('x-tag')
         b.innerText = `</${t}>`
 
         if (ignore.includes(t)) {
-          let aa = document.createElement('div')
-          c.after(aa)
-          aa.prepend(a, c)
-          let src = c.src || c.href
+          let aa = document.createElement('x-node')
+          el.dataset.orig = ''
+          el.before(aa)
+          aa.prepend(a, el)
+          let src = el.src || el.href
           if (src) {
             fetch(src).then(async res => {
               let txt = await res.text()
@@ -60,12 +64,22 @@
             })
           } else if (nonvoid.includes(t)) aa.append(b)
         } else {
-          c.prepend(a)
-          c.append(b)
+          el.prepend(a)
+          el.append(b)
         }
       }
     }
+  }
+
+  onMount(() => {
     scrollTo(0, 0)
+    let f = () => {
+      deco()
+      requestAnimationFrame(() => {
+        setTimeout(f, 1e4)
+      })
+    }
+    f()
   })
 </script>
 
@@ -73,7 +87,7 @@
   <title>barebones</title>
 </svelte:head>
 
-<h1>test</h1>
+<h1>loading...</h1>
 <img alt="test" src="https://i.imgur.com/w8pK4DY.png" />
 
 <style>
@@ -84,8 +98,12 @@
     @apply hidden;
   }
 
-  :global([data-tag]) {
+  :global(x-tag) {
     @apply text-gray;
+  }
+
+  :global(img) {
+    @apply max-screen;
   }
 
   :root {
