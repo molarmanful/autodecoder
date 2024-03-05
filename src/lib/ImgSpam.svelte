17f19f9806c318202b510c2ecc15e72e5813932f
molarmanful <benjinpang@gmail.com>
2024-03-05 14:50:53 -0500

    prog

---
+++ src/lib/ImgSpam.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let alt
+  export let src
+</script>
+
+<figure class="relative max-h-1/2 max-w-1/2">
+  <img class="max-full" {alt} loading="lazy" {src} />
+  <figcaption class="absolute inset-0 overflow-auto">
+    {#await fetch(src) then r}{#await r.text() then txt}{txt}{/await}{/await}
+  </figcaption>
+</figure>
