b8a0733327994f4a0ccdd9ad79fd318734f209c4
molarmanful <benjinpang@gmail.com>
2024-03-22 03:22:26 -0400

    better?

--- src/lib/SpamSet.svelte
+++ src/lib/SpamSet.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { ImgSpam, counter, randint } from '.'
+  import { ImgSpam, randint } from '.'
 
   export let a = 5
   export let b = 5
@@ -8,6 +8,8 @@
 {#each new Array(randint(b) + a) as _}
   <ImgSpam
     alt="test"
-    src={`https://source.unsplash.com/random/${(randint(12) + 1) * 50}x${(randint(12) + 1) * 50}#${Date.now()}`}
+    src={randint(2)
+      ? `https://source.unsplash.com/random/${(randint(12) + 1) * 50}x${(randint(12) + 1) * 50}#${Date.now()}`
+      : `https://picsum.photos/${(randint(12) + 1) * 50}/${(randint(12) + 1) * 50}`}
   />
 {/each}
